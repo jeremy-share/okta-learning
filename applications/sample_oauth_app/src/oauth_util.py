fb9c0b77300864108f6fe3a3b1467bbd2f36acd6
Jeremy Sells <mrjeremysells@gmail.com>
2023-08-31 22:14:18 +1200

    Adding in logic for keys. I tested it, and it works, but I am skeptical

--- applications/sample_oauth_app/src/oauth_util.py
+++ applications/sample_oauth_app/src/oauth_util.py
@@ -1,6 +1,77 @@
 import hashlib
 import os
+import time
+import uuid
+from jwcrypto import jwk
+
+import jwt
+# from cryptography.hazmat.backends import default_backend
+# from cryptography.hazmat.primitives.asymmetric import rsa
+# from cryptography.hazmat.primitives import serialization
 import base64
+
+
+# def base64url_to_int(value):
+#     """Converts a Base64URL encoded string to an integer."""
+#     padding = '=' * (4 - (len(value) % 4))
+#     return int.from_bytes(base64.urlsafe_b64decode(value + padding), 'big')
+#
+
+# def jwk_to_pem_2(jwk_dict: dict) -> str:
+#
+#     private_numbers = rsa.RSAPrivateNumbers(
+#         d=base64url_to_int(jwk_dict["d"]),
+#         p=base64url_to_int(jwk_dict["p"]),
+#         q=base64url_to_int(jwk_dict["q"]),
+#         dmp1=base64url_to_int(jwk_dict["dp"]),
+#         dmq1=base64url_to_int(jwk_dict["dq"]),
+#         iqmp=base64url_to_int(jwk_dict["qi"]),
+#         public_numbers=rsa.RSAPublicNumbers(
+#             e=base64url_to_int(jwk_dict["e"]),
+#             n=base64url_to_int(jwk_dict["n"])
+#         )
+#     )
+#     private_key = private_numbers.private_key(default_backend())
+#
+#     pem_key = private_key.private_bytes(
+#         encoding=serialization.Encoding.PEM,
+#         format=serialization.PrivateFormat.PKCS8,
+#         encryption_algorithm=serialization.NoEncryption()
+#     ).decode("utf-8")
+#
+#     return pem_key
+
+
+def jwk_to_pem(jwk_dict: dict):
+    key = jwk.JWK(**jwk_dict)
+    return key.export_to_pem(private_key=True, password=None).decode('utf-8')
+
+
+def generate_unique_jwt_id() -> str:
+    return str(uuid.uuid4())
+
+
+def create_signed_jwt(client_id: str, token_endpoint: str, private_key: dict, jwt_id: str) -> str:
+    # Convert JWK to PEM
+    private_key_pem = jwk_to_pem(private_key)
+
+    issue_at = int(time.time())
+
+    # This function constructs a JWT and signs it using the private key.
+    claims = {
+        "iss": client_id,
+        "sub": client_id,
+        "aud": token_endpoint,
+        "exp": issue_at + 600,  # Token validity, e.g., 5 minutes
+        "iat": issue_at,
+        "jti": jwt_id,
+        "kid": private_key["kid"]
+    }
+
+    # Sign the JWT with the RSA private key
+    signed_jwt = jwt.encode(claims, private_key_pem, algorithm=private_key["alg"])
+
+    return signed_jwt
 
 
 async def generate_pkce_code_verifier() -> str:
